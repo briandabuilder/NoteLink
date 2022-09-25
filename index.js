ccc00c389eb90bfc4b08e4f9353ee71bbef75cf5
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-09-24 18:38:13 -0700

    file name change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 let notes = [];
 
 app.get("/", function(req, res) {
-    res.render("index", {
+    res.render("home", {
         notes: notes
     });
 });
